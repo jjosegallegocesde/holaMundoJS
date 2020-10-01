0ccd07d39e6b6acecde23f725076994f383ea595
Juan <juan.gallegomesa@gmail.com>
2020-10-01 10:33:03 -0500

    se crea controlador

---
+++ controladorDOM.js
@@ -0,0 +1,9 @@
+//Refereciar etiquetas de HTMl con el DOM
+let tituloTarjeta2 = document.getElementById("titulo2");
+let audioVariable = document.getElementById("audio1");
+let botonPista1 = document.getElementById("boton1");
+let botonPista2 = document.getElementById("boton2");
+
+//Modificar el texto de una referencia
+tituloTarjeta2.textContent = "banda coldplay";
+console.log(audioVariable.textContent);
