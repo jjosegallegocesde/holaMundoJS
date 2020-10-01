bc626105a29d5128613bd19942ab0df602565bb8
Juan <juan.gallegomesa@gmail.com>
2020-10-01 10:39:50 -0500

    se controla audio

--- controladorDOM.js
+++ controladorDOM.js
@@ -7,3 +7,6 @@ let botonPista2 = document.getElementById("boton2");
 //Modificar el texto de una referencia
 tituloTarjeta2.textContent = "banda coldplay";
 console.log(audioVariable.textContent);
+
+//Modificar la fuente o el recurso que se carga en un tag de multimedia(audio,video,imagen)
+audioVariable.src = "img/audio2.mp3";
