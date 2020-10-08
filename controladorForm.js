bd8d7758fb0285c5b2918c8859a94a0e203c847b
Juan <juan.gallegomesa@gmail.com>
2020-10-08 09:51:28 -0500

    se valida formulario

--- controladorForm.js
+++ controladorForm.js
@@ -1,7 +1,10 @@
 //Referenciamos las etiquetas HTML a controlar:
 let cajaNombre = document.getElementById("nombre");
 let cajaApellido = document.getElementById("apellido");
+
 let botonEnviar = document.getElementById("boton");
+
+let ayuda1 = document.getElementById("ayuda1");
 
 //Verificar el contenido de las cajas:
 botonEnviar.addEventListener("click", validarFormulario);
@@ -12,9 +15,31 @@ function validarFormulario(evento) {
     evento.preventDefault();
 
     if (cajaNombre.value == "" && cajaApellido.value == "") {
-        alert("Debes diligenciar los campos");
 
+        cajaNombre.classList.add("is-invalid");
+        cajaApellido.classList.add("is-invalid");
+        ayuda1.textContent = "Campo obligatorio";
+
+    } else if (cajaNombre.value == "" && cajaApellido.value != "") {
+
+        cajaNombre.classList.add("is-invalid");
+        cajaApellido.classList.remove("is-invalid");
+        ayuda1.textContent = "Campo obligatorio";
+
+    } else if (cajaNombre.value != "" && cajaApellido.value == "") {
+
+        cajaNombre.classList.remove("is-invalid");
+        cajaApellido.classList.add("is-invalid");
+        ayuda1.textContent = "";
+
+    } else {
+        cajaNombre.classList.remove("is-invalid");
+        cajaApellido.classList.remove("is-invalid");
+        ayuda1.textContent = "";
+        //llamado al backend
     }
+
+
 
 
 
